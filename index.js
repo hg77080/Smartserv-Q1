ef7b875e0db2b985a8054765783b26521c633c84
Himanshu Gupta <hg77080@gmail.com>
2021-12-30 18:28:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+function performGetRequest1() {
+  var resultElement = document.getElementById("app");
+  resultElement.innerHTML = "";
+  axios
+    .get("https://s3.amazonaws.com/open-to-cors/assignment.json")
+    .then(function(response) {
+      resultElement.innerHTML = getdata(response);
+    })
+    .catch(function(error) {
+      resultElement.innerHTML = error;
+    });
+}
+function getdata(response) {
+  var products1 = JSON.stringify(response.data.products);
+  var products = JSON.parse(products1);
+  var tdata = "<table><th>Title</th><th>Price</th><th>Popularity</th>";
+  var keys = Object.keys(products);
+  var arr = [];
+  for (let i = 0; i < keys.length; i++) {
+    arr.push([
+      products[keys[i]].title,
+      products[keys[i]].price,
+      products[keys[i]].popularity
+    ]);
+  }
+  arr.sort(function(a, b) {
+    var valueA, valueB;
+    valueA = a[2];
+    valueB = b[2];
+    if (+valueA > +valueB) {
+      return -1;
+    } else if (+valueA < +valueB) {
+      return 1;
+    }
+    return 0;
+  });
+  for (let j = 0; j < arr.length; j++) {
+    tdata +=
+      "<tr><td>" +
+      arr[j][0] +
+      "</td><td>" +
+      arr[j][1] +
+      "</td><td> " +
+      arr[j][2]+
+      "</td>"
+      "</tr>";
+
+  }
+  tdata += "</table>";
+  return tdata;
+}
